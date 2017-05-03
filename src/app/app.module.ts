835a26f66041fe7972600528ee9b5d8fc2d6793b
pachauri <vaiby1111@yahoo.co.in>
2017-05-03 22:44:36 +0530

    setting up the routes and bootstrap css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { ItemsComponent } from './components/items/items.component';
 import { WidgetsComponent } from './components/widgets/widgets.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
-import {AlertModule} from "ng2-bootstrap";
+import { AlertModule } from "ng2-bootstrap";
+import {AppComponent} from "./app.component";
+
+const appRoutes: Routes = [
+  { path: 'items', component: ItemsComponent },
+  { path: 'widgets', component: WidgetsComponent },
+  { path: '', redirectTo: 'items', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
+    AppComponent,
     ItemsComponent,
     WidgetsComponent,
     ItemDetailComponent,
@@ -20,9 +29,10 @@ import {AlertModule} from "ng2-bootstrap";
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     AlertModule.forRoot()
   ],
   providers: [],
-  bootstrap: [ItemsComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
